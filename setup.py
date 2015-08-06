3108a0ff4b921cead5e00c56f2e091db999fadb0
nopoolcoin <nopoolcoin@126.com>
2015-08-06 11:06:41 +0800

    add2
    add2

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@ from distutils.core import setup, Extension
 memorycoin_momentum_module = Extension('memorycoin_momentum',
                                libraries = ['ssl', 'crypto'],
                                sources = ['momentummodule.c',
-							'groestl.c',
-							'keccak.c',
-							'unint256.h',
-							'hashblock.h',              
+                                          'groestl.c',
+                                          'keccak.c',
                                           'momentum.c'],
                                include_dirs=['.'])
 
 setup (name = 'memorycoin_momentum',
        version = '1.0',
-       description = 'Bindings for AES-NI momentum proof of work used by memorycoin',
+       description = 'Bindings for AES-NI momentum proof of work used by Memorycoin',
        ext_modules = [memorycoin_momentum_module])
